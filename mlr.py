1a46c480391d819313a1699a70aa07ca7d27153c
John Harvey <john.j.hrvy@gmail.com>
2026-01-11 15:21:51 +0000

    Loaded data set and began cleaning/removing columns.

--- mlr.py
+++ mlr.py
@@ -2,3 +2,8 @@ import numpy as np
 import matplotlib as plt # Importing useful libraries
 import pandas as pd
 
+df = pd.read_csv("premier_league_1819.csv") # Loading Data Set
+
+df = df.drop(df.columns[[2,3,4,5,270,269,268]], axis = 1) #Droppi
+
+print(df.columns.tolist())
