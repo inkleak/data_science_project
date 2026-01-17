5d2af840fb415fc2dcc629a5ffc6274ff87bd9bd
John Harvey <john.j.hrvy@gmail.com>
2026-01-17 15:19:14 +0000

    Old data set removed as not suitable (many missing values for variables). New data set obtained via Kaggle.

--- mlr.py
+++ mlr.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib as plt # Importing useful libraries
 import pandas as pd
 
-df = pd.read_csv("premier_league_1819.csv") # Loading Data Set
+df = pd.read_csv("data/raw/premier-player-23-24.csv") # Loading Data Set
 
-df = df.drop(df.columns[[2,3,4,5,270,269,268]], axis = 1) #Droppi
-
-print(df.columns.tolist())
+print(df.columns)
