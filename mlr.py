06527618255b659306b770975c7fec32eb23e072
John Harvey <john.j.hrvy@gmail.com>
2026-01-18 14:29:23 +0000

    Comparison between two variables added.

--- mlr.py
+++ mlr.py
@@ -5,15 +5,18 @@ import sklearn as sklearn
 
 df = pd.read_csv("data/raw/premier-player-23-24.csv") # Loading Data Set
 
-def pearson_coeff(x, y):
+def pearson_coeff(x, y, y_name):
     std_x, std_y = np.std(x), np.std(y)
     mean_x, mean_y, mean_xy = np.mean(x), np.mean(y), np.mean(x*y)
     cov_xy = mean_xy - (mean_x*mean_y)
     r = cov_xy / (std_x*std_y)
-    print(r)
+    print(str(x_search)," vs "+str(y_name)+ ". R = " +str(r)) #Outputs correlation coefficient values
 
-x = df['Age'] # Select your first variable in here using the table in the Notion file
-y = df['90s'] # Select your second variable in here using the table in the Notion file
+x_search = 'xG' #Change this depending on what you want to compare
+x = df[x_search] #Selects the appropriate column
 
-pearson_coeff(x, y)
-
+for col in df.columns:
+    if col!= x_search: #Excluding self-comparison
+        y = df[col]
+        if pd.api.types.is_numeric_dtype(y): #Makes sure variables are numerical in order for analysis
+            pearson_coeff(x,y, col) #Running correlation coefficients 
