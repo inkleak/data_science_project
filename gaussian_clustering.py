1bb2af569b3569af57defce4bd34a28b97a13907
John Harvey <john.j.hrvy@gmail.com>
2026-02-02 15:27:13 +0000

    Gaussian Clustering beginning to be implemented.

---
+++ gaussian_clustering.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def gaussclust(): #Defining function
+    return 0 # Will add function in
+
+df = pd.read_csv('/data/raw/premier-player-23-24.csv') # Initialising DataFrame
+
+
